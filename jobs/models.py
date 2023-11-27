f919b6482d167c8cc9084a5b17dcdef2d0d7631e
llkhombek Bekchanov <97522681+Ilkhombek0311@users.noreply.github.com>
2023-11-27 18:11:11 +0500

    models updates

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CreateJobs(models.Model):
+    user_id = models.IntegerField()
+    create_date = models.DateTimeField()
+    deadline_start = models.DateTimeField()
+    deadline_end = models.DateTimeField()
+    title = models.CharField(max_length=255)
+    descreption = models.TextField()
+    status = models.IntegerField()
+    update_date = models.DateTimeField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.title
